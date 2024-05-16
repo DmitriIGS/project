33d08a0d07a07d20126910fff4211a22d58c8509
DmitriIGS <lazutin.dm@yandex.ru>
2024-05-16 17:03:24 +0300

    Uncommented tests

--- tests/test_api.py
+++ tests/test_api.py
@@ -2,22 +2,22 @@ from tests.pages.api_requests import ApiRequests
 import allure
 
 
-# @allure.feature('Создание задачи, подзадачи Jira, списание времени')
-# def test_create_jira_issue(rand_number_for_entites):
-#     jira_issue = ApiRequests(rand_number_for_entites)
-#     jira_issue.create_jira_issue()
+@allure.feature('Создание задачи, подзадачи Jira, списание времени')
+def test_create_jira_issue(rand_number_for_entites):
+    jira_issue = ApiRequests(rand_number_for_entites)
+    jira_issue.create_jira_issue()
 
 
-# @allure.feature('Добавление комментария в задаче Jira')
-# def test_comment(rand_number_for_entites):
-#     comment = ApiRequests(rand_number_for_entites)
-#     comment.comment_issue()
+@allure.feature('Добавление комментария в задаче Jira')
+def test_comment(rand_number_for_entites):
+    comment = ApiRequests(rand_number_for_entites)
+    comment.comment_issue()
 
 
 @allure.feature('Создание задачи HD из почты')
 def test_create_task_email(rand_number_for_entites):
     task_mail = ApiRequests(rand_number_for_entites)
-    # task_mail.create_task_email()
+    task_mail.create_task_email()
     task_mail.check_task_creation()
 
 
